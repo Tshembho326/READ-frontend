129b16744e50c6b3e19c6013ecdd87e089cec5ec
Tshembho326 <xvmrhu001@myuct.ac.za>
2024-09-05 19:53:30 +0200

    Added functionality on the header

--- src/speech/templates/Header.jsx
+++ src/speech/templates/Header.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import { Home, BookOpen, BarChart2, User } from "lucide-react";
 import "../static/css/Header.css";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+
+  const navigate = useNavigate();
+
+  const handleNavigation = (path) => {
+    navigate(path);
+  };
+
   return (
     <header>
       <div className="header-content">
@@ -11,17 +19,17 @@ const Header = () => {
       </div>
       <div>
       <nav className="navigation-bar">
-        <div className="nav-item">
-          <span> <Home className="icon" /> Home </span>
+        <div className="nav-item"> 
+          <span onClick={() => handleNavigation('/home')}> <Home className="icon" /> Home </span>
         </div>
-        <div className="nav-item">
-          <span> <BookOpen className="icon" /> Library </span>
+        <div className="nav-item"> 
+          <span onClick={() => handleNavigation('/library')}> <BookOpen className="icon"  /> Library </span>
         </div>
-        <div className="nav-item">
-          <span> <BarChart2 className="icon" /> Progress </span>
+        <div className="nav-item"> 
+          <span onClick={() => handleNavigation('/progress')}> <BarChart2 className="icon"  /> Progress </span>
         </div>
-        <div className="nav-item">
-          <span> <User className="icon" /> Profile </span>
+        <div className="nav-item"> 
+          <span onClick={() => handleNavigation('/profile')}> <User className="icon" /> Profile </span>
         </div>
       </nav>
       </div>
