18a1af3773a60de2cac9dde7e3b916ff02f13d2f
Tshembho326 <xvmrhu001@myuct.ac.za>
2024-09-06 17:41:03 +0200

    Added the story fetch functionality

--- src/speech/templates/ReadingPage.jsx
+++ src/speech/templates/ReadingPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 import { useLocation } from 'react-router-dom'; // Import useLocation to access the passed state
 import CaptureAudio from './CaptureAudio';
+import Logo from '../static/images/reading.png';
 import '../static/css/ReadingPage.css';
 import Header from '../../llibrary/tamplates/Header';
 
@@ -15,9 +16,9 @@ const ReadingPage = () => {
         <title>Reading Page | READ</title>
       </Helmet>
       <Header />
-      <h1>Start Reading</h1>
       <div className="reading-page">      
         <h1>{story?.title}</h1>
+        <img src={Logo} alt="Logo" className="login-logo" />
         <div className="story-content">
           <p>{story?.content}</p> {/* Display story content */}
         </div>
